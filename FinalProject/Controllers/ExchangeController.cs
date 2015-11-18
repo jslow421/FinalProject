2c8e136b7a6a5a576107e1392f71541be1bcc7aa
jslow421 <jslowik@my.wctc.edu>
2015-11-18 13:32:55 -0600

    started exchange controller

---
+++ FinalProject/Controllers/ExchangeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        // GET: Exchange
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /Exchange/Browse
+        public ActionResult Browse(String category) {
+            
+
+            
+            return View();
+        }
+
+        //GET: /Exchange/Details
+        public ActionResult Details(int id) {
+            return View();
+        }
+    }
+}
