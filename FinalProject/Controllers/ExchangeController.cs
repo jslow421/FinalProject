b3055cd7efa7edcfdcc1b32c16285ee40f520c32
jslow421 <jslowik@my.wctc.edu>
2015-11-18 13:52:53 -0600

    exchangecontroller awaiting models

--- FinalProject/Controllers/ExchangeController.cs
+++ FinalProject/Controllers/ExchangeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FinalProject.Models;
 
 namespace FinalProject.Controllers
 {
@@ -10,21 +11,25 @@ namespace FinalProject.Controllers
     {
         // GET: Exchange
         public ActionResult Index()
+            //category model required to finish index controller
         {
             return View();
         }
 
         //GET: /Exchange/Browse
         public ActionResult Browse(String category) {
+            //need category model class to finish this
+            var categoryModel = new Category { };
             
-
-            
-            return View();
+            return View(categoryModel);
         }
 
         //GET: /Exchange/Details
         public ActionResult Details(int id) {
-            return View();
+            //need ticket model to complete this
+            var ticket = new Ticket {Title="Category " + id };
+
+            return View(ticket);
         }
     }
 }
