61f35997914a1c767e2616dcf0ab0ecf8849db26
ekordik <emmakordik@gmail.com>
2015-11-23 13:04:01 -0600

    Model Classes Added

---
+++ FinalProject/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.Models
+{
+    public class Category
+    {
+        public int categoryId { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public List<Ticket> tickets { get; set; }
+    }
+}
---
+++ FinalProject/Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.Models
+{
+    public class Ticket
+    {
+        public int ticketId { get; set; }
+        public string eventName { get; set; }
+        public string location { get; set; }
+        public DateTime date { get; set; }
+        public DateTime time { get; set; }
+        public string tradeType { get; set; }
+        public string user { get; set; }
+        public Category category { get; set; }
+    }
+}
