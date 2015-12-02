b08eb26dfa5cb50737e751e0107ad96f30f9be44
Timothy Rogers <TRogers8@my.wctc.edu>
2015-12-01 23:28:06 -0600

    added admin controller and views.  still need to do the identity migration and updated configuration page for it

---
+++ FinalProject/DataContexts/IdentityDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using FinalProject.Models;
+using FinalProject.DataContexts;
+
+namespace FinalProject
+{
+    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDb() : base("DefaultConnection") { }
+        public static IdentityDb Create()
+        {
+            return new IdentityDb();
+        }
+    }
+}
